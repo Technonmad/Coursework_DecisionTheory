155944dc312cb064fea1f7a1b8b8c9a2b95afd14
Anton <65555937+Technonmad@users.noreply.github.com>
2021-04-04 17:36:11 +0300

    123

---
+++ kursa4/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace kursa4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Интервал:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(77, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(42, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Готово";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(329, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 157);
+            this.panel1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Затраты на новое оборудование:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(329, 207);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 262);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(248, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Решения";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Остаточная стоимость:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 41);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Помощь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(326, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Таблица решений";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(137, 259);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(107, 160);
+            this.listBox1.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Итоговая политика замены";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Возраст оборудования:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 22;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1, 428);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 41);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Выход";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 478);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Оптимальный план замены оборудования";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ kursa4/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kursa4
+{
+    public partial class Form1 : Form
+    {
+        int x = 10;
+        int y = 10;
+        int val;
+        int age;
+
+        TextBox[,] mas;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && textBox1.Text != "" && comboBox1.SelectedItem != null && textBox3.Text != "")
+            {
+                try
+                {
+                    if (Convert.ToInt32(textBox1.Text) > 0 && Convert.ToInt32(textBox2.Text) > 0)
+                    {
+                        if (Convert.ToInt32(textBox2.Text) > Convert.ToInt32(textBox1.Text) && Convert.ToInt32(textBox3.Text) <= Convert.ToInt32(comboBox1.SelectedItem))
+                        {
+                            button2.Enabled = true;
+                            button3.Enabled = true;
+                            button1.Enabled = false;
+
+                            val = Convert.ToInt32(comboBox1.Text);
+                            mas = new TextBox[val + 2, 3];
+
+                            for (int i = 0; i < val + 2; i++)
+                            {
+                                for (int j = 0; j < 3; j++)
+                                {
+                                    mas[i, j] = new TextBox();
+                                    mas[i, j].Location = new Point(x + 50 * i, y + 50 * j);
+                                    mas[i, j].Name = i + " " + j;
+                                    mas[i, j].Size = new Size(40, 20);
+                                    panel1.Controls.Add(mas[i, j]);
+                                    mas[0, 0].Enabled = false;
+
+                                    for (int f = 1; f < val + 2; f++)
+                                    {
+                                        if (j == 0 && i == f)
+                                        {
+                                            mas[i, j].Text = Convert.ToString(i - 1);
+                                            mas[i, j].Enabled = false;
+                                        }
+                                    }
+                                }
+                            }
+                            mas[0, 1].Text = "R ( t )";
+                            mas[0, 2].Text = "r ( t )";
+                            mas[0, 1].Enabled = false;
+                            mas[0, 2].Enabled = false;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Стоимость покупки не может быть выше стоимости продажи");
+                            textBox1.Clear();
+                            textBox2.Clear();
+                        } 
+                    }
+                    else MessageBox.Show("Введите целое неотрицательное число");
+                }
+                catch
+                {
+                    MessageBox.Show("Введите число, а не что вы там ввели");
+                }
+                
+            }
+            else MessageBox.Show("Вы ввели не все значения");          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            button1.Enabled = true;
+            button3.Enabled = false;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                listBox1.Items.Clear();
+
+                int check = 0;
+
+
+                val = Convert.ToInt32(comboBox1.Text);
+
+                for (int i = 1; i < val + 2; i++)
+                {
+                    for (int j = 1; j < 3; j++)
+                    {
+                        if (mas[i, j].Text == "") check = 1;
+                    }
+                }
+
+                if (check != 1)
+                {
+                    for (int i = 0; i <= val; i++)
+                    {
+                        dataGridView1.Columns.Add("S" + (i), "S" + (i));
+                    }
+
+                    dataGridView1.Rows.Add(val - 1);
+
+                    for (int i = 0; i < val; i++)
+                    {
+                        dataGridView1.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                    }
+                    dataGridView1.RowHeadersWidth = 50;
+
+                    int izd = Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text) + Convert.ToInt32(mas[1, 1].Text) - Convert.ToInt32(mas[1, 2].Text);
+
+                    int[] razn = new int[val + 1];
+
+                    for (int i = 1; i < val + 2; i++)
+                    {
+                        int prib = Convert.ToInt32(mas[i, 1].Text) - Convert.ToInt32(mas[i, 2].Text);
+                        razn[i - 1] = prib;
+
+                        if (prib.CompareTo(izd) > 0)
+                        {
+                            dataGridView1[i - 1, 0].Value = prib;
+                            dataGridView1[i - 1, 0].Style.ForeColor = Color.Green;
+                        }
+                        if (prib.CompareTo(Math.Abs(izd)) < 0)
+                        {
+                            dataGridView1[i - 1, 0].Value = izd;
+                            dataGridView1[i - 1, 0].Style.ForeColor = Color.Red;
+                        }
+                        if (prib.CompareTo(Math.Abs(izd)) == 0)
+                        {
+                            dataGridView1[i - 1, 0].Value = prib;
+                            dataGridView1[i - 1, 0].Style.ForeColor = Color.Green;
+                        }
+
+                    }
+
+                    int pribnext;
+
+                    for (int i = 1; i < val; i++)
+                    {
+
+
+                        int izdnext = izd + Convert.ToInt32(dataGridView1[1, i - 1].Value);
+
+
+                        for (int j = 0; j < val + 1; j++)
+                        {
+                            pribnext = razn[j] + Convert.ToInt32(dataGridView1[j + 1, i - 1].Value);
+
+                            if (pribnext >= izdnext)
+                            {
+                                dataGridView1[j, i].Value = pribnext;
+                                dataGridView1[j, i].Style.ForeColor = Color.Green;
+                            }
+                            else
+                            {
+                                dataGridView1[j, i].Value = Math.Abs(izdnext);
+                                dataGridView1[j, i].Style.ForeColor = Color.Red;
+                                break;
+                            }
+                        }
+                    }
+
+                    age = Convert.ToInt32(textBox3.Text);
+                    int year;
+                    int b;
+
+                    if (dataGridView1[age, val - 1].Style.ForeColor == Color.Red || Convert.ToString(dataGridView1[age, val - 1].Value) == "")
+                    {
+                        listBox1.Items.Add(val + ")" + " " + "Замена");
+                        b = 1;
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(val + ")" + " " + "Сохранение");
+                        b = age;
+                    }
+
+                    year = val - 1;
+
+                    for (int j = val - 2; j >= 0; j--)
+                    {
+                        if (dataGridView1[b, j].Style.ForeColor == Color.Red)
+                        {
+                            listBox1.Items.Add((year) + ")" + " " + "Замена");
+                            b = 1;
+                            year--;
+                        }
+                        else
+                        {
+                            listBox1.Items.Add((year) + ")" + " " + "Сохранение");
+                            year--;
+                            b++;
+                        }
+                    }
+                }
+                else MessageBox.Show("Неправильно введены данные");
+            }
+            catch
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                MessageBox.Show("Введены некорректные данные");
+            }
+            
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Для начала выберите нужный вам временной промежуток в списке 'Интервал'." + "\n" +
+                "Затем введите затраты на новое оборудование, остаточную стоимость(доход с продажи старого оборудования) и возраст вашего оборудования. Нажмите кнопку 'Готово'" + "\n" +
+                "В появившейся таблице введите доход с продажи продукции, производимой на оборудовании, в год ( R(t) )" + "\n" +
+                "и расходы, связанные с этим оборудованием (в год) ( r (t) )." + "\n" +
+                "Затем нажмите на кнопку 'Решения'." + "\n" + "\n" +
+                "! Обратите внимание, что вы не сможете изменить параметры таблицы пока не нажмете на кнопку 'Очистить' !" + "\n" + "\n" +
+                "Выведется таблица с возрастом оборудования и доходом от него (строки - год из интервала, а столбцы - возраст оборудования)." + "\n" +
+                "Также, рядом выведется оптимальная политика замены оборудования." + "\n" +
+                "Таким образом вы сможете определить максимальную прибыль за каждый год вашего интервала, а также определить оптимальную политику замены оборудования.");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Спасибо за пользование моей программой!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Спасибо за пользование моей программой!");
+        }
+    }
+}
